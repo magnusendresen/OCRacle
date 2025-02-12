de4073069811c7303b372d507ad1d6e7d027edba
Magnus Endresen <magnus.stensby.endresen@gmail.com>
2025-02-12 23:05:56 +0100

    chatgpt

--- scripts/gptapi.py
+++ scripts/gptapi.py
@@ -17,9 +17,11 @@ nonchalant = "Ikke forklar hva du gjør, bare gjør det."
 def main(text):
     amount = int(promptToText("Hvor mange oppgaver er det i denne teksten som er extractet? Svar kun med ett tall som skal omgjøres til en int og brukes til programmering: " + text))
     print("Antall oppgaver: " + str(amount))
+    print("Det er omtrent", round(len(text) / 5), "ord i teksten fra pdf-en.")
     tasks = []
     for i in range(amount):
         task = promptToText(nonchalant + "Hva er oppgave " + str(i+1) + "?: Skriv all tekst knyttet til oppgaven rett fra råteksten, ikke løs oppgaven: " + text)
+        print("Oppgave ", i+1, " er omtrent ", round(len(task) / 5), " ord lang.")
         task = promptToText(nonchalant + "Fjern all tekst knyttet til Inspera og eksamensgjennomførelse: " + task)
         task = promptToText(nonchalant + "Oversett oppgaven til norsk bokmål: " + task)
         task = promptToText(nonchalant + "Skriv all teksten over på en enkelt linje altså uten noen newlines: " + task)
